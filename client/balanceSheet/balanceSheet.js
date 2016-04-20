dbb43c440a433255c18900b5241b298518c152e0
Kayle Dev <driver@kayle.me>
2016-04-20 11:09:57 -0600

    adding balance template

--- client/balanceSheet/balanceSheet.js
+++ client/balanceSheet/balanceSheet.js
@@ -1,5 +1,13 @@
 Template.balanceSheet.helpers({
-
+'totalIncome': function(){
+return 
+},
+    'totalExpenses':function(){
+        return
+    },
+'totalStats':function(){
+    return
+}
 });
 
 Template.balanceSheet.events({
